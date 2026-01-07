b40b0b1a3450b482a3e9dca0274b343d38e800a9
ci-robot <ci-robot@k8s.io>
2026-01-07 18:13:29 +0000

    Update to ACK runtime `v0.57.0`, code-generator `v0.57.0`

--- pkg/resource/event_data_store/delta.go
+++ pkg/resource/event_data_store/delta.go
@@ -17,16 +17,15 @@ package event_data_store
 
 import (
 	"bytes"
-	"reflect"
 
 	ackcompare "github.com/aws-controllers-k8s/runtime/pkg/compare"
 	acktags "github.com/aws-controllers-k8s/runtime/pkg/tags"
+	"k8s.io/apimachinery/pkg/api/equality"
 )
 
 // Hack to avoid import errors during build...
 var (
 	_ = &bytes.Buffer{}
-	_ = &reflect.Method{}
 	_ = &acktags.Tags{}
 )
 
@@ -47,7 +46,7 @@ func newResourceDelta(
 	if len(a.ko.Spec.AdvancedEventSelectors) != len(b.ko.Spec.AdvancedEventSelectors) {
 		delta.Add("Spec.AdvancedEventSelectors", a.ko.Spec.AdvancedEventSelectors, b.ko.Spec.AdvancedEventSelectors)
 	} else if len(a.ko.Spec.AdvancedEventSelectors) > 0 {
-		if !reflect.DeepEqual(a.ko.Spec.AdvancedEventSelectors, b.ko.Spec.AdvancedEventSelectors) {
+		if !equality.Semantic.Equalities.DeepEqual(a.ko.Spec.AdvancedEventSelectors, b.ko.Spec.AdvancedEventSelectors) {
 			delta.Add("Spec.AdvancedEventSelectors", a.ko.Spec.AdvancedEventSelectors, b.ko.Spec.AdvancedEventSelectors)
 		}
 	}
